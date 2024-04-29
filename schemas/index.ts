48e2e4d49f1e24ac9cac5f5a4bccbc648f34133e
Yash Mehta <yashmehta0395@gmail.com>
2024-04-29 11:12:37 +0530

    Added Schemas for Password and email resetting

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,46 @@
 import * as z from "zod";
 
+export const SettingsSchema = z.object({
+    name: z.optional(z.string()),
+    isTwoFactorEnabled: z.optional(z.string()),
+    role: z.enum([UserRole.ADMIN, UserRole.USER]),
+    email: z.optional(z.string().email()),
+    password: z.optional(z.string().min(6)),
+    newPassword: z.optional(z.string().min(6)),
+})
+.refine((data) => {
+    if (data.password && !data.newPassword) {
+        return false;
+    }
+
+    return true;
+}, {
+    message: "New Password is Required",
+    path: ["newPassword"]
+})
+.refine((data) => {
+    if (data.newPassword && !data.password) {
+        return false;
+    }
+
+    return true;
+}, {
+    message: "Current Password is Required",
+    path: ["password"]
+});
+
+export const NewPasswordSchema = z.object({
+    passwrod: z.string().min(6, {
+        message: "Minimum 6 characters required"
+    }),
+});
+
+export const ResetSchema = z.object({
+    email: z.string().email({
+        message: "Email is Required"
+    }),
+});
+
 export const LoginSchema = z.object({
     email: z.string().email({
         message: "Email is Required"
