8af16012cd2d535788175710f9bc1ad5740d1cd4
Mohit Mehta <mehtam2701@gmail.com>
2024-05-23 10:33:59 +0530

    updated metadata

--- app/(dashboard)/app/page.tsx
+++ app/(dashboard)/app/page.tsx
@@ -7,7 +7,6 @@ export default function Home() {
     const { user } = useCurrentUser();
     return (
         <div>
-            Home
             <div>{user && `Hi ${user.email}!`}</div>
             <Button onClick={() => signOut()}>Sign out</Button>
         </div>
