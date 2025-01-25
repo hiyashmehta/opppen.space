0499ed556ffb40f55f8d134bd62261e41402e77e
Mohit Mehta <mehtam2701@gmail.com>
2025-01-25 20:40:07 +0530

    added ui components,

--- src/app/(dashboard)/[slug]/layout.tsx
+++ src/app/(dashboard)/[slug]/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <SidebarProvider>
             <CreateCommunityDialog />
             <AppSidebar />
-            <SidebarInset className="px-8 py-6">
+            <SidebarInset className="">
                 {/* <header className="flex h-16 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
                     <div className="flex items-center gap-2 px-4">
                         <SidebarTrigger className="-ml-1" />
---
+++ src/server/api/routers/members.ts
@@ -0,0 +1,97 @@
+import { z } from "zod";
+
+import {
+    createTRPCRouter,
+    protectedProcedure,
+    publicProcedure,
+} from "@/server/api/trpc";
+
+export const membersRouter = createTRPCRouter({
+    hello: publicProcedure
+        .input(z.object({ text: z.string() }))
+        .query(({ input }) => {
+            return {
+                greeting: `Hello ${input.text}`,
+            };
+        }),
+
+    getSecretMessage: protectedProcedure.query(() => {
+        return "you can now see this secret message!";
+    }),
+    createCommunity: protectedProcedure
+        .input(
+            z.object({
+                name: z.string().min(3),
+                slug: z.string(),
+                description: z.string(),
+            }),
+        )
+        .mutation(async ({ ctx, input }) => {
+            const { name, slug, description } = input;
+            const user = ctx.session.user;
+            if (!user) {
+                throw new Error("You must be logged in to create a community");
+            }
+            const community = await ctx.db.community.create({
+                data: {
+                    name,
+                    slug,
+                    description,
+
+                    privacy: "PRIVATE",
+                    user: {
+                        connect: {
+                            id: user.id,
+                        },
+                    },
+                },
+            });
+            return community;
+        }),
+    getUserCommunity: protectedProcedure
+        .input(
+            z.object({
+                communityId: z.string(),
+            }),
+        )
+        .query(({ ctx, input }) => {
+            const communityId = input.communityId;
+            const user = ctx.session.user;
+
+            if (!user) {
+                throw new Error("You must be logged in to get a community");
+            }
+            const community = ctx.db.community.findUnique({
+                where: {
+                    id: communityId,
+                    userId: user.id,
+                },
+            });
+            return community ?? {};
+        }),
+
+    getAllCommunities: protectedProcedure.query(({ ctx }) => {
+        const user = ctx.session.user;
+
+        if (!user) {
+            throw new Error("You must be logged in to get a community");
+        }
+        const communities = ctx.db.community.findMany({
+            where: {
+                userId: user.id,
+            },
+        });
+        return communities ?? [];
+    }),
+    getPublicCommunity: publicProcedure
+        .input(z.object({ communityId: z.string() }))
+        .query(({ input, ctx }) => {
+            const communityId = input.communityId;
+            const community = ctx.db.community.findUnique({
+                where: {
+                    id: communityId,
+                },
+            });
+            return community ?? {};
+        }),
+});
